040b9cd60393642249986f61da3dcdb3e45ecd83
Shuvam747 <shuvamdhakal20@gmail.com>
2024-12-18 17:23:31 +0545

    Add project files.

---
+++ PersonalFinanceApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalFinanceApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ PersonalFinanceApplication/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PersonalFinanceApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
